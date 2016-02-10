93d1b31fa2b20cb44515ac9bb92ce25f1572a1cd
Daniela <daniela.galeote@gmail.com>
2016-02-10 15:34:44 +0000

    Set timeout

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ var synth = new Tone.SimpleSynth().toMaster();
 
 function playSound(tone) {
   synth.triggerAttackRelease(tone, "8n");
+  console.log('trigger');
 }
 
 function emitSound(tone) {
@@ -15,4 +16,30 @@ socket.on('play note', function(tone) {
   playSound(tone);
   $('#playing').text(tone);
   console.log('client played note');
+
 });
+
+function playSong() {
+  track = {
+    1000: 'C4',
+    2000: 'D4',
+    3000: 'C4',
+    5000: 'C4',
+
+  };
+  console.log('playingsong');
+
+  play(track);
+}
+
+function play(track) {
+  $.each(track, function(time,note) {
+    console.log('begplayingnote');
+    setTimeout(function(){
+      playSound(note);
+      console.log('playingnote');
+    },time);
+
+  });
+
+}
