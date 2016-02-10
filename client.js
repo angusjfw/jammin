9ceba1ff5008ae8832354f07a6c548e48a0fe223
Sara Tateno <sara.tateno@gmail.com>
2016-02-10 16:01:28 +0000

    Records a played note

--- client.js
+++ client.js
@@ -1,10 +1,40 @@
-var socket = io();
+// stopwatch
+
+StopWatch = function(){
+    this.reset();
+};
+
+StopWatch.prototype.startTime = function(){
+    this.startMilliseconds = new Date().getTime();
+    console.log("Recording started");
+};
+
+StopWatch.prototype.markTime = function(){
+    return new Date().getTime() - s1.startMilliseconds;
+};
+
+StopWatch.prototype.reset = function(){
+  this.startMilliseconds = 0;
+  this.elapsedMilliseconds = 0;
+};
+
+// tone.js
 
 var synth = new Tone.SimpleSynth().toMaster();
 
+var s1 = new StopWatch();
+var recording = {};
+
+
 function playSound(tone) {
   synth.triggerAttackRelease(tone, "8n");
+  var elapsedTime = s1.markTime();
+  recording[elapsedTime] = tone;
+  console.log(recording);
 }
+
+// websockets
+var socket = io();
 
 function emitSound(tone) {
   socket.emit('transmit note', tone);
